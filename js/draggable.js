70607d0ff038243aa50b9c53792e08783aeba9a7
juancotera <juancoteracastro@gmail.com>
2022-05-13 19:23:44 -0500

    definiendo estado inicial del widget

--- js/draggable.js
+++ js/draggable.js
@@ -1,5 +1,5 @@
 const defaultConfig = {
-  open: true,
+  open: false,
   debug: true,
   animatable: true,
 }
@@ -8,10 +8,39 @@ export default function draggable($element, config = defaultConfig) {
   if (!($element instanceof HTMLElement)) {
     return console.warn(`Elemento invalido se esparaba un HTMLElement y se recibió ${$element}`)
   }
+  let isOpen = config.open
+  const elementRect = $element.getBoundingClientRect()
+  const ELEMENT_BLOCK_SIZE = elementRect.height
+
+  const $marker = $element.querySelector('[data-marker]')
+  const MARKER_BLOCK_SIZE = $marker.getBoundingClientRect().height
+
+  const VISIBLE_Y_POSITION = 0
+  const HIDDEN_Y_POSITION = ELEMENT_BLOCK_SIZE - MARKER_BLOCK_SIZE
+  let widgetPosition = VISIBLE_Y_POSITION
+  isOpen ? open() : close() //consultamos con un ternario si es open que vaya a open o si no que vaya a close
 
   function logger(message) {
     if (config.debuge) {
       console.info(message)
     }
+  }
+
+  function open() {
+    logger('Abrir widget')
+    isOpen = true
+    widgetPosition = VISIBLE_Y_POSITION
+    setWidgetPosition(widgetPosition)
+  }
+
+  function close() {
+    logger('Cerrar widget')
+    isOpen = false
+    widgetPosition = HIDDEN_Y_POSITION
+    setWidgetPosition(widgetPosition)
+  }
+
+  function setWidgetPosition(value) {
+    $element.style.marginBottom = `-${value}px`
   }
 }
