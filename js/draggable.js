176dc07e4807c00e75c6e65f819bb72a3f80b77b
juancotera <juancoteracastro@gmail.com>
2022-05-13 19:40:57 -0500

    definiendo eventos de interacción

--- js/draggable.js
+++ js/draggable.js
@@ -1,5 +1,5 @@
 const defaultConfig = {
-  open: false,
+  open: true,
   debug: true,
   animatable: true,
 }
@@ -9,9 +9,9 @@ export default function draggable($element, config = defaultConfig) {
     return console.warn(`Elemento invalido se esparaba un HTMLElement y se recibió ${$element}`)
   }
   let isOpen = config.open
+  let isDragging = false
   const elementRect = $element.getBoundingClientRect()
   const ELEMENT_BLOCK_SIZE = elementRect.height
-
   const $marker = $element.querySelector('[data-marker]')
   const MARKER_BLOCK_SIZE = $marker.getBoundingClientRect().height
 
@@ -19,6 +19,53 @@ export default function draggable($element, config = defaultConfig) {
   const HIDDEN_Y_POSITION = ELEMENT_BLOCK_SIZE - MARKER_BLOCK_SIZE
   let widgetPosition = VISIBLE_Y_POSITION
   isOpen ? open() : close() //consultamos con un ternario si es open que vaya a open o si no que vaya a close
+
+  $marker.addEventListener('click', handleClick)
+  $marker.addEventListener('pointerdown', handlePointerDown)
+  $marker.addEventListener('pointerup', handlePointerUp)
+  $marker.addEventListener('pointerout', handlePointerOut)
+  $marker.addEventListener('pointercancel', handlePointerCancel)
+  $marker.addEventListener('pointermove', handlePointerMove)
+
+
+  function handlePointerUp() {
+    logger('Pointer Up')
+
+  }
+
+  function handlePointerOut() {
+    logger('Pointer OUT')
+  }
+
+  function handlePointerCancel() {
+    logger('Pointer Cancel')
+
+  }
+
+  function handlePointerDown() {
+    logger('Pointer Down')
+
+  }
+
+
+  function handleClick(event) {
+    logger('CLICK')
+    toggle()
+  }
+
+  function handlePointerMove() {
+    logger('Pointer MOVE')
+
+  }
+
+  function toggle() {
+    if (!isDragging) {
+      if (!isOpen) {
+        return open()
+      }
+      return close()
+    }
+  }
 
   function logger(message) {
     if (config.debuge) {
